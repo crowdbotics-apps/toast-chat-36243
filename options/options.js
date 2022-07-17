5fab72cc1c64393dddf8f952bdcdf35207163a0e
Crowdbotics <team@crowdbotics.com>
2022-07-17 17:55:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "toast_chat_36243",
+  url: "https://toast_chat_36243.botics.co",
+  api: "https://toast_chat_36243.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
